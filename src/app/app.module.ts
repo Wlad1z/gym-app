4a8ef7355c8e50174f405609387a1ed9c8e7a6b7
Wlad1z <113522178+Wlad1z@users.noreply.github.com>
2024-07-29 22:06:15 +0300

    add 18v

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './components/screens/home/home.module';
 import { HeaderModule } from './components/ui/header/header.module';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { TrainingComponent } from './components/screens/training/training.component';
 import { CheckboxComponent } from './components/ui/checkbox/checkbox.component';
 import { LoginModule } from './components/screens/login/login.module';
@@ -13,30 +13,19 @@ import { ExercisComponent } from './components/screens/training/exercis/exercis.
 import { ExercisElementComponent } from './components/screens/training/exercis-element/exercis-element.component';
 import { ExercisChangeComponent } from './components/screens/training/exercis-change/exercis-change.component';
 
-
-
-
-
-
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    TrainingComponent,
-    CheckboxComponent,
-    ExercisComponent,
-    ExercisElementComponent,
-    ExercisChangeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HomeModule,
-    HeaderModule,
-    HttpClientModule,
-    LoginModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        TrainingComponent,
+        CheckboxComponent,
+        ExercisComponent,
+        ExercisElementComponent,
+        ExercisChangeComponent
+    ],
+    bootstrap: [AppComponent], 
+    imports: [BrowserModule,
+        AppRoutingModule,
+        HomeModule,
+        HeaderModule,
+        LoginModule], 
+    providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
